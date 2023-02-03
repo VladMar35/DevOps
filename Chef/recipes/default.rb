f89b53a5b933f0f95efafdef07ae1bd9690233a4
Vladyslav Marchenko <111642735+vvmarchenko@users.noreply.github.com>
2023-02-03 10:53:27 +0200

    Update default.rb

--- Chef/recipes/default.rb
+++ Chef/recipes/default.rb
@@ -1,6 +1,12 @@
-mysql_service 'mysql' do
+# Configure the MySQL client.
+mysql_client 'default' do
+  action :create
+end
+mysql_service 'default' do
+  version '5.5'
+  bind_address '0.0.0.0'
   port '3306'
-  version '8.0'
-  initial_root_password 'your_strong_password'
+  data_dir '/data'
+  initial_root_password "password123"
   action [:create, :start]
 end
